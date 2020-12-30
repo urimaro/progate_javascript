16aa3abf130f5e843dd1d4c2e5fe40507d894483
Goh Matsumoto <urimaro@gmail.com>
2020-12-31 00:30:10 +0900

    8. Scope - 1. Learning Functions

---
+++ javascript_iii/src/8_script.js
@@ -0,0 +1,13 @@
+// 定数nameを定義してください
+const name = "にんじゃわんこ";
+
+const introduce = (name) => {
+  // 「わたしは〇〇です」を出力してください
+  console.log(`わたしは${name}です`);
+};
+
+// 関数introduceを呼び出してください
+introduce("ひつじ仙人");
+
+// 定数nameの値を出力してください
+console.log(name);
