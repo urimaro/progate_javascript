ba5c8b85042439949e7eb2abaa28fd125d968758
Goh Matsumoto <urimaro@gmail.com>
2020-12-31 00:37:50 +0900

    9. Final Exercise - 1. Learning Functions

---
+++ javascript_iii/dist/9_script.js
@@ -0,0 +1,20 @@
+const number1 = 103;
+const number2 = 72;
+const number3 = 189; // getMax関数を定義してください
+
+const getMax = (a, b, c) => {
+  let max = a;
+
+  if (b > max) {
+    max = b;
+  }
+
+  if (c > max) {
+    max = c;
+  }
+
+  return max;
+}; // 「最大値は○○です」と出力してください
+
+
+console.log(`最大値は${getMax(number1, number2, number3)}です`);
