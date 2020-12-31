3644a9e9cd87da45f72b30a35c50bee62a14cdb7
Goh Matsumoto <urimaro@gmail.com>
2020-12-31 15:56:58 +0900

    10. What is Inheritance? - 2. Class Inheritance

---
+++ javascript_iv/dist/10_script.js
@@ -0,0 +1,22 @@
+class Animal {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet() {
+    console.log("こんにちは");
+  }
+
+  info() {
+    this.greet();
+    console.log(`名前は${this.name}です`);
+    console.log(`${this.age}歳です`);
+  }
+
+}
+
+class Dog extends Animal {}
+
+const animal = new Animal("レオ", 3);
+animal.info();
