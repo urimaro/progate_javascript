30111d52ff1f35816b78cb01ca4912498ca09b3f
Goh Matsumoto <urimaro@gmail.com>
2021-01-01 23:28:37 +0900

    1. Separating Files (1) - 1. Separating Files

---
+++ javascript_v/src/animal.js
@@ -0,0 +1,16 @@
+class Animal {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet() {
+    console.log("こんにちは");
+  }
+
+  info() {
+    this.greet();
+    console.log(`名前は${this.name}です`);
+    console.log(`${this.age}歳です`);
+  }
+}
---
+++ javascript_v/src/script.js
@@ -0,0 +1,2 @@
+const dog = new Dog("レオ", 4, "チワワ");
+dog.info();
