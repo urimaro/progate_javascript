2e900ea08f470feca5ffc3d6090652d4b2e8a5fd
Goh Matsumoto <urimaro@gmail.com>
2021-01-04 16:14:29 +0900

    7. Packages (2) - 2. Using Packages

--- javascript_v/src/data/dogData.js
+++ javascript_v/src/data/dogData.js
@@ -1,6 +1,12 @@
+import readlineSync from "readline-sync";
 import Dog from "../class/dog.js";
 
 const dog1 = new Dog("レオ", 4, "チワワ");
-const dog2 = new Dog("ベン", 2, "プードル");
+
+const name = readlineSync.question("名前を入力してください: ");
+const age = readlineSync.questionInt("年齢を入力してください: ");
+const breed = readlineSync.question("犬種を入力してください: ");
+
+const dog2 = new Dog(name, age, breed);
 
 export {dog1, dog2};
